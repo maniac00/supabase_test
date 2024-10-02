02a0d4a751afd463490c42aeaaba231a972fea82
Sungwook Kim <7wario@gmail.com>
2024-10-02 19:58:59 +0900

    first compleate

--- api/main.py
+++ api/main.py
@@ -1,13 +1,3 @@
-import os
-from supabase import create_client, Client
-
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
-
-
-
-
 from fastapi import FastAPI
 
 app = FastAPI()
